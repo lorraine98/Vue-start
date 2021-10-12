0a4f485f06d1af49f365a55507c0c12ea5e8eec7
lorraine98 <hyosung0425@naver.com>
2021-10-13 02:42:48 +0900

    add package

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,47 @@
-const path = require('path')
-const { VueLoaderPlugin } = require('vue-loader')
-const HtmlPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    resolve: {
-        extensions: ['.vue', '.js'],
-        alias: {
-            '~': path.resolve(__dirname, 'src')
-        }
+  resolve: {
+    extensions: [".vue", ".js"],
+    alias: {
+      "~": path.resolve(__dirname, "src"),
     },
-    entry: './src/main.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-        //생략시 기본 이름으로 설정됨.
-        //filenmae:'hello.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                use: 'vue-loader'
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new VueLoaderPlugin(),
-        new HtmlPlugin({
-            template: 'src/index.html'
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: 'static' } //to 옵션은 output의 path로 기본값을 가져 생략 가능.
-            ]
-        })
-    ]
-}
+  },
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    clean: true,
+    //생략시 기본 이름으로 설정됨.
+    //filenmae:'hello.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.s?css$/,
+        use: ["vue-style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlPlugin({
+      template: "src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "static" }, //to 옵션은 output의 path로 기본값을 가져 생략 가능.
+      ],
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
